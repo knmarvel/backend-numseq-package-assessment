ee0305b516ff08c53037585c88ffb9c6052e8f16
Karen Marvel <knmarvel@gmail.com>
2020-03-16 20:07:11 -0400

    working on prime optimization

--- numseq/prime.py
+++ numseq/prime.py
@@ -1,16 +1,25 @@
 def primes(n):
+    """given a number, n, returns all prime numbers less than n"""
     import math
     if n < 2:
         return []
     else:
-        primes_list = [2]
-        for num in range(3, n, 2):
-            if all(num % i != 0 for i in range(3, int(math.sqrt(num))+1, 2)):
-                primes_list.append(num)
-        return primes_list
+        primes = [2]
+        if n <= primes[-1]:
+            return primes
+        else:
+            for index in range(3, n, 2):
+                isPrime = True
+                for primenumber in primes:
+                    if not index % primenumber:
+                        isPrime = False
+                if isPrime:
+                    primes.append(index)
 
 
 def is_prime(n):
+    """given a number, n, returns true if the number is prime and\
+false if it's not"""
     primes_list = primes(n+1)
     print(primes_list)
     return n in primes_list
